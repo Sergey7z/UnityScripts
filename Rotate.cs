073040c9cd3c8dfa46aaec5e580a0760a3f59c1c
Sergio <42730175+Sergey7z@users.noreply.github.com>
2022-02-24 05:58:17 +0300

    Update Rotate.cs

--- Rotate.cs
+++ Rotate.cs
@@ -5,20 +5,21 @@ using UnityEngine;
 using System.Collections;
 
 public class Rotate : MonoBehaviour {
-    float positionX, positionY, positionZ;
-    public float rotationSpeed;
-    public sbyte aroundX, aroundY, aroundZ;
+    float positionX, positionY, positionZ; // Позиция вращения
+    public float rotationSpeed; // Скорость вращения
+    public sbyte aroundX, aroundY, aroundZ; // Направление вращения
     
     void Update() {
         
-        positionX = gameObject.transform.position.x;
-        positionY = gameObject.transform.position.y;
-        positionZ = gameObject.transform.position.z;
+        positionX = gameObject.transform.position.x; // Получение позиции вращения от объекта
+        positionY = gameObject.transform.position.y; // к которому прикреплен скрипт,
+        positionZ = gameObject.transform.position.z; // т.е. свои собственные координаты
     
         transform.RotateAround (new Vector3(positionX, positionY, positionZ),
                                 new Vector3(aroundX, aroundY, aroundZ),
                                 rotationSpeed * Time.deltaTime);
 
+        // Это не обязательно, можно удалить
         if (aroundX > 1) aroundX = 1;
         if (aroundX < -1) aroundX = -1;
         if (aroundY > 1) aroundY = 1;
