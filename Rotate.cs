f58a7addf6d6126291c16bd96452d05cf674b4c8
Sergio <42730175+Sergey7z@users.noreply.github.com>
2022-02-24 05:31:25 +0300

    Rotation of an object around its center

---
+++ Rotate.cs
@@ -0,0 +1,29 @@
+// Вращение объекта вокруг своего центра
+// Rotation of an object around its center
+
+using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+    float positionX, positionY, positionZ;
+    public float rotationSpeed;
+    public sbyte aroundX, aroundY, aroundZ;
+    
+    void Update() {
+        
+        positionX = gameObject.transform.position.x;
+        positionY = gameObject.transform.position.y;
+        positionZ = gameObject.transform.position.z;
+    
+        transform.RotateAround (new Vector3(positionX, positionY, positionZ),
+                                new Vector3(-1 * aroundZ, aroundY, aroundX),
+                                rotationSpeed * Time.deltaTime);
+
+        if (aroundX > 1) aroundX = 1;
+        if (aroundX < -1) aroundX = -1;
+        if (aroundY > 1) aroundY = 1;
+        if (aroundY < -1) aroundY = -1;
+        if (aroundZ > 1) aroundZ = 1;
+        if (aroundZ < -1) aroundZ = -1;
+    }
+}
