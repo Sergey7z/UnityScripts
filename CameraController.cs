f4d53310cf59d5beed2a4fefd970abe5b2480f00
Sergio <42730175+Sergey7z@users.noreply.github.com>
2022-02-27 19:26:28 +0300

    Update CameraController.cs

--- CameraController.cs
+++ CameraController.cs
@@ -6,15 +6,15 @@ public class CameraController : MonoBehaviour
 {
     Vector3 rot;
     public float speedMove = 5f;
-    public float speedRotX = 0f,
-                 speedRotY = 0f;
+    public float speedRotX = 1f;  // Не должно быть 0
+    public float speedRotY = 1f;  // Не должно быть 0
 
     void Start(){
         Cursor.visible = false;
-        
     }
 
     void Update(){
+        
         if (speedRotX > 0){
             rot.x = Input.mousePosition.x * speedRotX;
         }
@@ -27,7 +27,7 @@ public class CameraController : MonoBehaviour
         if (speedRotY < 0){
             rot.y = Input.mousePosition.y / speedRotY;
         }
-        
+
         transform.rotation = Quaternion.Euler (rot.y, rot.x, 0);
         
         if(Input.GetKey(KeyCode.W)){
@@ -42,8 +42,7 @@ public class CameraController : MonoBehaviour
         if(Input.GetKey(KeyCode.D)){
             transform.position += new Vector3(speedMove * Time.deltaTime, 0, 0);
         }
-        
-
- 
     }
 }
+
+
