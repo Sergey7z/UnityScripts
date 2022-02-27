c6b0640954cf8efe909cf8069ff8405389246bc8
Sergio <42730175+Sergey7z@users.noreply.github.com>
2022-02-27 18:30:20 +0300

    Update CameraController.cs

--- CameraController.cs
+++ CameraController.cs
@@ -5,17 +5,45 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     Vector3 rot;
-    public float speed = 5f;
+    public float speedMove = 5f;
+    public float speedRotX = 0f,
+                 speedRotY = 0f;
 
     void Start(){
         Cursor.visible = false;
+        
     }
 
     void Update(){
+        if (speedRotX > 0){
+            rot.x = Input.mousePosition.x * speedRotX;
+        }
+        if (speedRotY > 0){
+            rot.y = Input.mousePosition.y * -speedRotY;
+        }
+        if (speedRotX < 0){
+            rot.x = Input.mousePosition.x / -speedRotX;
+        }
+        if (speedRotY < 0){
+            rot.y = Input.mousePosition.y / speedRotY;
+        }
         
-        rot.x = Input.mousePosition.x / speed;
-        rot.y = Input.mousePosition.y / speed;
-        transform.rotation = Quaternion.Euler(-rot.y, rot.x, 0);
+        transform.rotation = Quaternion.Euler (rot.y, rot.x, 0);
+        
+        if(Input.GetKey(KeyCode.W)){
+            transform.position += new Vector3(0, 0, speedMove * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.S)){
+            transform.position -= new Vector3(0, 0, speedMove * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.A)){
+            transform.position -= new Vector3(speedMove * Time.deltaTime, 0, 0);
+        }
+        if(Input.GetKey(KeyCode.D)){
+            transform.position += new Vector3(speedMove * Time.deltaTime, 0, 0);
+        }
+        
+
  
     }
 }
